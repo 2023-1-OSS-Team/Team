33a0dd96eddb4c8768f06dffe9ee2c475573f351
Jingburgerlove <kods901056@gmail.com>
2023-05-14 23:20:55 +0900

    다이어리 전체틀 구현 함수 생성

---
+++ printForm.c
@@ -0,0 +1,16 @@
+#include "BibleDiary.h"
+
+void printForm()
+{
+    int i, j = 0;
+
+    for(i = 0; i < 80; i++)
+    {
+        pr("▣");
+        if(i == 0 || i == 79)
+        {
+            for(j = 0; j > -79; j--)
+                pr("\n▣");
+        }
+    }
+}
