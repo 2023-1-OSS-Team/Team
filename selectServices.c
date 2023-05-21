e8880458f77048040c19b4aaf7c1ba7ccf590c6b
Eden.Yang <114277398+prorege@users.noreply.github.com>
2023-05-21 22:16:25 +0900

    Update selectServices.c
    기능명 세분화 및 기능 출력명 변경

--- selectServices.c
+++ selectServices.c
@@ -1,17 +1,17 @@
 #include "BibleDiary.h"
 
-int selectFunction()
+int selectServices()
 {
     int menu;
     printf("\n*** 성경 묵상 일기 작성 ***\n");
-    printf("1. 새로운 일기 작성하기\n");
-    printf("2. 작성한 일기 확인하기\n");
-    printf("3. 작성한 일기 수정하기\n");
-    printf("4. 작성한 일기 삭제하기\n");
-    printf("5. 저장된 일기 확인하기\n");
-    printf("6. 저장된 일기 편집하기\n");
-    printf("7. 성경 말슴 검색하기\n");
-    printf("8. 오늘의 추천 말씀\n"); // 월간정산 불러올 지, 일간정산 불러올 지 입력 받고 하기.
+    printf("1. 오늘 일기 쓰기\n");
+    printf("2. 오늘 일기 보기\n");
+    printf("3. 오늘 일기 수정\n");
+    printf("4. 오늘 일기 삭제\n");
+    printf("5. 오늘 일기 저장\n");
+    printf("6. 지난 일기 보기\n");
+    printf("7. 지난 일기 수정\n");
+    printf("8. 지난 일기 삭제\n"); // 월간정산 불러올 지, 일간정산 불러올 지 입력 받고 하기.
     printf("0. 일기 종료하기\n\n");
     printf(" >> 원하시는 메뉴를 선택하세요: ");
     scanf("%d", &menu);
